3e12cae001483f419182e5792d612f6753623ca2
Sthefany <sthefany.cedeno@est.fi.uncoma.edu.ar>
2024-08-15 09:26:40 -0300

    cree un ejemplo y agrege un nuevo archivo

---
+++ ejemplo/ejemplo.java
@@ -0,0 +1,11 @@
+public class Contador {
+    private int cuenta = 0;
+
+    public synchronized void incrementar() {
+        cuenta++;
+    }
+
+    public int getCuenta() {
+        return cuenta;
+    }
+}
